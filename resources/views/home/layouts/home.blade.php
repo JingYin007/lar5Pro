d782da1bc3960120299b82e0bdb4632e52487c14
moTzxx <JingYin007@users.noreply.github.com>
2017-12-24 04:33:10 +0800

    work-js
    work 页面优化

--- resources/views/home/layouts/home.blade.php
+++ resources/views/home/layouts/home.blade.php
@@ -14,26 +14,7 @@
             $('.quake-slider').quake({ thumbnails: false, frameWidth: 600, frameHeight: 609 });
         });
     </script>
-
-
-    <script type="text/javascript" src="{{asset('home/jquery.fancybox-1.3.4/fancybox/jquery.mousewheel-3.0.4.pack.js')}}"></script>
-    <script type="text/javascript" src="{{asset('home/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js')}}"></script>
-    <link rel="stylesheet" type="text/css" href="{{asset('home/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css')}}"
-          media="screen" />
-    <script type="text/javascript">
-        $(document).ready(function () {
-
-            $("a[rel=example_group]").fancybox({
-                'transitionIn': 'none',
-                'transitionOut': 'none',
-                'titlePosition': 'over',
-                'titleFormat': function (title, currentArray, currentIndex, currentOpts) {
-                    return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-                }
-            });
-
-        });
-    </script>
+    @yield('work-js')
     <!--slider end-->
 </head>
 <body>
