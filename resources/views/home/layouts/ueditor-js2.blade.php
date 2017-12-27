45b031ee9a0d90d05de1efeedd42d80abcd521e3
moTzxx <JingYin007@users.noreply.github.com>
2017-12-27 20:17:21 +0800

    ok

--- resources/views/home/layouts/ueditor-js2.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- ueditor-mz 配置文件 -->
-<script type="text/javascript" charset="utf-8" src="{{asset('laravel-u-editor/ueditor.config.js')}}"></script>
-<!-- 编辑器源码文件 -->
-<script type="text/javascript" charset="utf-8" src="{{asset('laravel-u-editor/ueditor.all.min.js')}}"></script>
-<script type="text/javascript" charset="utf-8" src="{{asset('laravel-u-editor/lang/zh-cn/zh-cn.js')}}"></script>
-
-<!-- 实例化编辑器 -->
-
