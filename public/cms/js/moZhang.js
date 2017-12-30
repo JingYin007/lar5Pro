d43e3a72fdc2eef0199272e573b4441893b15406
moTzxx <JingYin007@users.noreply.github.com>
2017-12-30 14:30:29 +0800

    layui_footer
    layui_footer 动效添加

--- public/cms/js/moZhang.js
+++ public/cms/js/moZhang.js
@@ -8,14 +8,21 @@ $(document).ready(function () {
         var leftView = $(".layui-bg-black");
         var hidden = leftView.is(':hidden');
         var layui_body = $(".layui-layout-admin .layui-body");
+        var layui_footer = $(".layui-layout-admin .layui-footer");
         leftView.animate({width: 'toggle'});
         if(hidden){
             layui_body.animate({
                 left:parseInt(layui_body.css('left'),200) == 200 ? +layui_body.outerWidth() : 200
             });
+            layui_footer.animate({
+                left:parseInt(layui_footer.css('left'),200) == 200 ? +layui_footer.outerWidth() : 200
+            });
         }else {
             layui_body.animate({
                 left:parseInt(layui_body.css('left'),10) == 0 ? -layui_body.outerWidth() : 0
+            });
+            layui_footer.animate({
+                left:parseInt(layui_footer.css('left'),10) == 0 ? -layui_footer.outerWidth() : 0
             });
         }
     });
