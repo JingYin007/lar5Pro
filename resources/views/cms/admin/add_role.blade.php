ca01b68de030d7578bfaeec9af3266585f0c9b09
moTzxx <JingYin007@users.noreply.github.com>
2018-02-02 16:47:55 +0800

    ok

--- resources/views/cms/admin/add_role.blade.php
+++ resources/views/cms/admin/add_role.blade.php
@@ -22,31 +22,29 @@
 
         <div class="layui-form-item layui-form-text">
             <label class="layui-form-label">权限分配：</label>
+            <input type="hidden" name="nav_menu_ids" class="nav_menu_ids" value="">
             <div class="layui-input-block">
                 <table class="layui-table table-nav-menus">
                     <colgroup>
-                        <col width="20%">
-                        <col width="20%">
-                        <col width="20%">
+                        <col width="10%">
                         <col>
                     </colgroup>
                     <tbody>
                     @foreach($menus as $vo)
                         <tr>
-                            @foreach($vo['child'] as $voi)
-                                <td><input type="checkbox" name="" title="{{$voi['name']}}"></td>
-                            @endforeach
+                            <td class="td-menu-parent td-nav-menu">
+                                {{$vo['name']}}
+                                <input type="checkbox" class="cb-nav-menu" name="" value="{{$vo['id']}}" lay-skin="switch" lay-text="ON|OFF" checked>
+                            <td class="td-menu-child td-nav-menu">
+                                @foreach($vo['child'] as $voi)
+                                    <input type="checkbox" class="cb-nav-menu" name="" value="{{$voi['id']}}" title="{{$voi['name']}}" checked>
+                                @endforeach
+                            </td>
                         </tr>
                     @endforeach
-
-
                     </tbody>
                 </table>
             </div>
-
-
-
-
         </div>
 
         <div class="layui-form-item">
@@ -64,11 +62,42 @@
     <script src="{{asset('cms/js/roles.js')}}"></script>
     <script src="{{asset('cms/js/moZhang.js')}}"></script>
     <script>
-
+        /**
+         * 添加新角色
+         */
         function addNewRole() {
+            getNavmenuIDs();
             var postData = $(".form-opRoles").serialize();
             var toUrl = "{{url('cms/admin/addRole')}}";
             ToPostPopupsDeal(toUrl,postData);
+        }
+        /**
+         * 子级别菜单的选择
+         */
+        $(".table-nav-menus .td-menu-child").click(function () {
+            getNavmenuIDs();
+        });
+        //父级菜单的选择
+        $(".table-nav-menus .td-menu-parent").click(function () {
+            //判断当前是否选中状态
+            var tag = $(this).children("input[type='checkbox']").is(':checked')
+            if(!tag){
+                //TODO 此时为未选中状态
+                $(this).next().children(".layui-form-checkbox").removeClass("layui-form-checked");
+            }else {
+                $(this).next().children(".layui-form-checkbox").addClass("layui-form-checked");
+            }
+        });
+        /**
+         * 获取所有被选中的导航菜单
+         */
+        function getNavmenuIDs() {
+            var navmenuIDs = "";
+            $(".table-nav-menus input:checkbox:checked").each(function(i)
+            {
+                navmenuIDs += $(this).val()+",";
+            });
+            $(".nav_menu_ids").val(navmenuIDs);
         }
     </script>
 @endsection
